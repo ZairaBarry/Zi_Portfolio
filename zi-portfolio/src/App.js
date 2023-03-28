730ac1a1f23c0b133d7b4a57fa8884988c2c9b06
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-03-29 01:48:09 +0300

    initial commit

---
+++ zi-portfolio/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      < Header />
+      <Footer />
+      <Navbar />
+  </div>
+  
+  );
+}
+
+export default App;
