83b9f0e5fea7859e590cad1efa172105ee25d1e7
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-03-29 01:55:57 +0300

    created about.js

---
+++ zi-portfolio/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Hi. My name is Zi. I am Full-Stack Developer. I just completed UNB Coding Bootcamp and design code and beauty. I love what I do and I hope you will love it too. Tou can see some of my projects in the portfolio section.
+      </p>
+    </div>
+  );
+}
