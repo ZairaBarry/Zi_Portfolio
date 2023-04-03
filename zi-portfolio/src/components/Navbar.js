52e26c509bcd875f354bd2ab6367c20bb2ee08a3
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-04-03 19:49:48 +0300

    created README

--- zi-portfolio/src/components/Navbar.js
+++ zi-portfolio/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Navbar({ currentPage, handlePageChange }) {
     return (
-      <ul className="nav justify-content-end">
+      <ul className="nav justify-content-center">
         <li className="nav-item">
           <a
             href="#About"
--- zi-portfolio/src/components/pages/About.js
+++ zi-portfolio/src/components/pages/About.js
@@ -9,7 +9,7 @@ export default function About() {
       <div class="card text-bg-dark mb-3" width={50} height={50}>
         <div class="card-header ">About me</div>
         <div class="card-body">
-          <h5 class="card-title">Hi. Welcome to my page. My anme is Zaira and I am newly graduated from UNB Coding Bootcamp Full Stack Developer. I love coding and I love the magic behind it. It my page you can view my projects and if you like them you can contact me by clicking Contact form. </h5>
+          <h5 class="card-title">Hi. Welcome to my page. My name is Zaira and I am newly graduated from UNB Coding Bootcamp Full Stack Developer. I love coding and I love the magic behind it. It my page you can view my projects and if you like them you can contact me by clicking Contact form. </h5>
          
         </div>
       </div>
